f87c2072bd4f6c10b6cbd99de4ec5b775c8a92de
hhjo <kikiki0611@naver.com>
2019-01-10 02:48:47 +0900

    Refactor run_server to run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from webServer.RestServer import RestServer
 
+# todo : load data from .ini file
 if __name__ == "__main__":
     server = RestServer('127.0.0.1', 8000)
-    server.run_server()
+    server.run()
