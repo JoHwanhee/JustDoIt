fad6c802bf84c98fd501e7e54dc171ed3e139b5a
hhjo <kikiki0611@naver.com>
2019-01-10 02:41:45 +0900

    Add json header

--- api/restUtils.py
+++ api/restUtils.py
@@ -7,3 +7,8 @@ def set_access_control(response):
     response.set_header("Access-Control-Allow-Origin", _allow_origin)
     response.set_header("Access-Control-Allow-Methods", _allow_methods)
     response.set_header("Access-Control-Allow-Headers", _allow_headers)
+
+
+def set_header_json(response):
+    response.set_header("Content-Type", "application/json; charset=utf-8")
+    response.set_header("Cache-Control", "no-cache")
